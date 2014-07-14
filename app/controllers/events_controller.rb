6247db861b520da53bb351631125c2fe525f129e
helenswanson <hhyswanson@gmail.com>
2014-07-13 23:35:47 -0400

    New event view page displays properly

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,28 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
   end
 
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      flash[:notice] = 'Success! Your event is live.'
+    else
+      flash.now[:notice] = 'Error. Please try again.'
+      render :new
+    end
+  end
+
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :category1, :category2, :category3,
+      :description, :available_seats, :start_time, :end_time)
   end
 end
