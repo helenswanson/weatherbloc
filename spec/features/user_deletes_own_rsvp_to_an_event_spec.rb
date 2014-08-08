e6fde52d321c4c1073ca76ffdd618b6153afa91a
helenswanson <hhyswanson@gmail.com>
2014-08-08 05:44:00 -0400

    Specs pass for user deleting own rsvp to an event.

--- spec/features/user_deletes_own_rsvp_to_an_event_spec.rb
+++ spec/features/user_deletes_own_rsvp_to_an_event_spec.rb
@@ -13,7 +13,16 @@ feature 'user deletes own RSVP to an event', %Q(
 # - I need to see a success message if my RSVP is successful.
 # - I need to see an error message if RSVP is unsuccessful.
 
+  scenario 'attendee removes own rsvp' do
+    @user = FactoryGirl.create(:user)
+    login(@user)
+    @event = FactoryGirl.create(:event)
+    visit event_path(@event)
 
+    click_button 'RSVP'
+    click_button 'Withdraw RSVP'
 
-
+    expect(page).to have_css('#rsvp-button')
+    expect(page).to_not have_button('Withdraw RSVP')
+  end
 end
